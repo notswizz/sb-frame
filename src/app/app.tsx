158149833ea3d01f9c8e95baffced3b53e854feb
Jack Smith <jsmith@ISEML-JSm.local>
2025-02-02 00:51:40 -0500

    ok

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import dynamic from "next/dynamic";
+import Demo from '~/components/Demo';
 
-const Demo = dynamic(() => import("~/components/Demo"), {
+const DemoComponent = dynamic(() => import("~/components/Demo"), {
   ssr: false,
 });
 
-export default function App(
-  { title }: { title?: string } = { title: "Frames v2 Demo" }
-) {
-  return <Demo title={title} />;
+export default function App() {
+  return <Demo />;
 }
